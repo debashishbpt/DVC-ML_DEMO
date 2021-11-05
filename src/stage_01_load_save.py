b7249db0b149dd5463e8db632ba437eeb8ec45e9
debashishbpt <61720939+debashishbpt@users.noreply.github.com>
2021-11-05 10:53:02 -0500

    Stage 01 created

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -1,6 +1,26 @@
-from src.utils.all_utils import read_yaml
+from src.utils.all_utils import create_directory, read_yaml
 import argparse
 import pandas as pd
+import os
+
+def get_data(config_path):
+    config = read_yaml(config_path)
+
+    remote_data_path = config["data_source"]
+    df = pd.read_csv(remote_data_path, sep=";")
+    
+    #Save dataset in local directory
+    #Create path to directory: artifacts/raw_local_dir/data.csv
+
+    artifacts_dir = config["artifacts"]["artifacts_dir"]
+    raw_local_dir = config["artifacts"]["raw_local_dir"]
+    raw_local_file = config["artifacts"]["raw_local_file"]
+
+    raw_local_dir_path = os.path.join(artifacts_dir, raw_local_dir)
+    create_directory(dirs=[raw_local_dir_path])
+    raw_local_file_path = os.path.join(raw_local_dir_path, raw_local_file)
+    df.to_csv(raw_local_file_path, sep=",", index=False)
+    
 
 
 if __name__ == '__main__':
@@ -11,4 +31,4 @@ if __name__ == '__main__':
     parsed_args = args.parse_args()
 
 
-    print(parsed_args)
+    get_data(config_path=parsed_args.config)
